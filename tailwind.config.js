50810d9a84ed7647d4736adc58188536e95cf287
Tariq A <tachorzyad@gmail.com>
2022-11-03 21:24:56 -0500

    Updated Resume

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'shark': '#1c2324',
+        },
+
+
+      },
     },
     plugins: [],
   }
